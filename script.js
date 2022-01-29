b7ad904d2602de29770595fdaefbee9a5ab19f5f
ThinThinzarHtet <thinthinzarhtet19@gmail.com>
2022-01-29 20:13:02 +0630

    add change theme, add items, remove items and count function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// Change Theme
 
+
+
+// Change Theme
 const themeIcon = document.querySelector('.change-theme');
 const body = document.querySelector('body');
 
@@ -17,3 +19,64 @@ themeIcon.addEventListener('click', function(){
         document.querySelector('.bgphoto').style.transition = "all 300ms";
     }
 })
+
+//Add Items
+const todoInput = document.getElementById('current-todo');
+const addBtn = document.querySelector('.check');
+
+addBtn.addEventListener('click', () => {
+    if (todoInput.value.length > 0) {
+        addItems(todoInput.value); //add function
+        todoInput.value = '';
+    }
+})
+todoInput.addEventListener('keypress', (event) => {
+    if (event.key === "Enter" && todoInput.value.length > 0 ) {
+        addItems(todoInput.value);
+        todoInput.value = '';
+    }
+})
+
+// Add Item Functions
+const ul = document.querySelector('.todos');
+function addItems(todo) {
+    const li = document.createElement('li');
+    li.setAttribute('class', 'list-wrapper');
+    ul.appendChild(li);
+    li.innerHTML = `
+        <input type="checkbox" />
+        <span>${todo}</span>
+        <img src="./images/icon-cross.svg" class="remove" alt="cross">`;
+    updateCount(1);
+}
+
+
+// remove item
+ul.addEventListener('click', deleteCheck);
+
+function deleteCheck(e) {
+    const item = e.target;
+    //Delete Todo
+    if(item.classList[0] === 'remove') {
+        const todo = item.parentElement;
+        // console.log(todo);
+        todo.remove();
+        removeCount(1);
+    }
+}
+
+// Items Count
+const itemsCount = document.querySelector('.footer .items-left span');
+const itemsCountMob = document.querySelector('.mob .items-left span');
+itemsCount.innerText = document.querySelectorAll('.list-wrapper').length;
+itemsCountMob.innerText = document.querySelectorAll('.list-wrapper').length;
+
+function updateCount(num) {
+    itemsCount.innerText = +itemsCount.innerText + num;
+    itemsCountMob.innerText = +itemsCountMob.innerText + num;
+}
+
+function removeCount(num) {
+    itemsCount.innerText = +itemsCount.innerText - num;
+    itemsCountMob.innerText = +itemsCountMob.innerText - num;
+}
